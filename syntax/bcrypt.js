48cd4ebbddf66f49b3e97808373748f79b255243
정우진 <jeong-ujin@jeong-ujin-ui-MacBook-Pro.local>
2020-03-29 03:39:34 +0900

    Multi user

--- syntax/bcrypt.js
+++ syntax/bcrypt.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const myPlaintextPassword = "111111";
 const someOtherPlaintextPassword = "111112";
+
 bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
   // Store hash in your password DB.
   console.log(hash);
